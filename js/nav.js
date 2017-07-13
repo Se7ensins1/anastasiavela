febc3bfb5a5bc4a8b7033762d514a2c081604dbe
Anastasia Vela <anastasiavela@berkeley.edu>
2017-07-13 15:54:55 -0700

    add pics from imgur

--- js/nav.js
+++ js/nav.js
@@ -42,22 +42,3 @@ function openAbout() {
     document.getElementById("navAbout").style.width = "45%";
     moveTitle();
 }
-
-
-
-$(".container").each(function(){
-    // Uncomment the following if you need to make this dynamic
-    var refH = $(this).height();
-    var refW = $(this).width();
-    var refRatio = refW/refH;
-    
-    
-    var imgH = $(this).children("img").height();
-    var imgW = $(this).children("img").width();
-    
-    if ( (imgW/imgH) < refRatio ) { 
-        $(this).addClass("portrait");
-    } else {
-        $(this).addClass("landscape");
-    }
-})
