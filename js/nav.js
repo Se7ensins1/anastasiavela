305fa4be6ee4ecc986b661538ffe5101aa3f47e3
Se7ensins1 <anastasiavela@berkeley.edu>
2017-08-10 14:05:53 -0700

    add logo

--- js/nav.js
+++ js/nav.js
@@ -44,7 +44,6 @@ function openAbout() {
     disappear();
 }
 
-
 function resizeImages() {
 
     for(i=1; i <= 12; i++) {
@@ -74,4 +73,126 @@ function show() {
 
 function disappear() {
     document.getElementById("containers").style.display = "none";
+}
+
+
+
+function onePop() {
+    document.getElementById("1pop").style.display = "inline";
+    for(i=1; i<=12; i++) {
+        if (i != 1) {
+            var id = i.toString() + "pop"
+            document.getElementById(id).style.display = "none";
+        }
+    }
+}
+
+function twoPop() {
+    document.getElementById("2pop").style.display = "inline";
+    for(i=1; i<=12; i++) {
+        if (i != 2) {
+            var id = i.toString() + "pop"
+            document.getElementById(id).style.display = "none";
+        }
+    }
+}
+
+function threePop() {
+    document.getElementById("3pop").style.display = "inline";
+    for(i=1; i<=12; i++) {
+        if (i != 3) {
+            var id = i.toString() + "pop"
+            document.getElementById(id).style.display = "none";
+        }
+    }
+}
+
+function fourPop() {
+    document.getElementById("4pop").style.display = "inline";
+    for(i=1; i<=12; i++) {
+        if (i != 4) {
+            var id = i.toString() + "pop"
+            document.getElementById(id).style.display = "none";
+        }
+    }
+}
+
+function fivePop() {
+    document.getElementById("5pop").style.display = "inline";
+    for(i=1; i<=12; i++) {
+        if (i != 5) {
+            var id = i.toString() + "pop"
+            document.getElementById(id).style.display = "none";
+        }
+    }
+}
+
+function sixPop() {
+    document.getElementById("6pop").style.display = "inline";
+    for(i=1; i<=12; i++) {
+        if (i != 6) {
+            var id = i.toString() + "pop"
+            document.getElementById(id).style.display = "none";
+        }
+    }
+}
+
+function sevenPop() {
+    document.getElementById("7pop").style.display = "inline";
+    for(i=1; i<=12; i++) {
+        if (i != 7) {
+            var id = i.toString() + "pop"
+            document.getElementById(id).style.display = "none";
+        }
+    }
+}
+
+function eightPop() {
+    document.getElementById("8pop").style.display = "inline";
+    for(i=1; i<=12; i++) {
+        if (i != 8) {
+            var id = i.toString() + "pop"
+            document.getElementById(id).style.display = "none";
+        }
+    }
+}
+
+function ninePop() {
+    document.getElementById("9pop").style.display = "inline";
+    for(i=1; i<=12; i++) {
+        if (i != 9) {
+            var id = i.toString() + "pop"
+            document.getElementById(id).style.display = "none";
+        }
+    }
+}
+
+function tenPop() {
+    document.getElementById("10pop").style.display = "inline";
+    for(i=1; i<=12; i++) {
+        if (i != 10) {
+            var id = i.toString() + "pop"
+            document.getElementById(id).style.display = "none";
+        }
+    }
+}
+
+function elevenPop() {
+    document.getElementById("11pop").style.display = "inline";
+    for(i=1; i<=12; i++) {
+        if (i != 11) {
+            var id = i.toString() + "pop"
+            document.getElementById(id).style.display = "none";
+        }
+    }
+}
+
+function twelvePop() {
+    document.getElementById("12pop").style.display = "inline";
+    for(i=1; i<=12; i++) {
+        if (i != 12) {
+            var id = i.toString() + "pop"
+            document.getElementById(id).style.display = "none";
+        }
+    }
 }
