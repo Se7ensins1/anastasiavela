37763c347a246a1d6dfc470cc122a18406bc7b63
Se7ensins1 <anastasiavela@berkeley.edu>
2018-04-18 09:20:19 -0700

    change front

---
+++ js/chevron.js
@@ -0,0 +1,4 @@
+function chevron() {
+	document.getElementById('menu').style.marginLeft = "0";
+	document.getElementById('chevron').style.display = "none"
+}
--- js/quote.js
+++ js/quote.js
@@ -1,9 +1,17 @@
 var captionElement;
 var captions = [
-	"If we are uncritical we shall always find what we want: we shall look for, and find, confirmations, and we shall look away from, and not see, whatever might be dangerous to our pet theories.",
-	"What screws us up the most in life is the picture in our head of how it is supposed to be. - unknown",
-	"We travel, some of us forever, to seek other states, other lives, other souls. - Anais Nin",
-	"You don`t find love, it finds you. It’s got a little bit to do with destiny, fate, and what’s written in the stars. -Anais Nin"
+	"\"If we are uncritical we shall always find what we want.\" \n -Karl Popper",
+	"\"What screws us up the most in life is the picture in our head of how it is supposed to be.\" \n-unknown",
+	"\"We travel, some of us forever, to seek other states, other lives, other souls.\" \n-Anais Nin",
+	"\"You don`t find love, it finds you. It`s got a little bit to do with destiny, fate, and what`s written in the stars.\" \n-Anais Nin",
+	"\"To do two things at once is to do neither.\" \n -Publilius Syrus",
+	"\"If you wish to be out front, then act as if you were behind.\" \n -Lao Tzu",
+	"\"You can`t depend on your eyes when your imagination is out of focus.\" \n -Mark Twain",
+	"\"A friend to all is a friend to none.\" \n -Aristotle",
+	"\"If you are lonely when you`re alone, you are in bad company.\" \n -Jean-Paul Sartre",
+	"\"The changes in our life must come from the impossibility to live...\" \n -Leo Tolstoy",
+	"\"The system is rigged against you.\" ",
+	"\"Ask yourself are you better off now than you were four years ago?\" -Ronald Regan"
 ];
 
 window.onload = function() {
