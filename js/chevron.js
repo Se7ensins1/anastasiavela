bc55502eeb4151b99c2a52507b18eb9cdeb71bf3
Se7ensins1 <anastasiavela@berkeley.edu>
2018-04-20 20:52:44 -0700

    mobile accommodation

--- js/chevron.js
+++ js/chevron.js
@@ -1,8 +1,3 @@
-function chevron() {
-	document.getElementById('menu').style.marginLeft = "0";
-	document.getElementById('chevron').style.display = "none"
-}
-
 $(document).ready(function(){
     $('#chevron').on('click touchstart', function() {
     	$("#menu").css("margin-left", "0px");
