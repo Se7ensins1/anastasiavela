c1c251503225d5795b22d42bd7de3ab1e9ca1a10
Se7ensins1 <anastasiavela@berkeley.edu>
2018-04-20 19:31:34 -0700

    chevron onclick

--- js/chevron.js
+++ js/chevron.js
@@ -2,3 +2,10 @@ function chevron() {
 	document.getElementById('menu').style.marginLeft = "0";
 	document.getElementById('chevron').style.display = "none"
 }
+
+$(document).ready(function(){
+    $('#chevron').on('click touchstart', function() {
+    	$("#menu").css("margin-left", "0px");
+    	$("#chevron").css("display", "none");
+    });
+});
