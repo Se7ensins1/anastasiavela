ceda522b2c5dd902bc3922b54b136d6c1318fded
Se7ensins1 <anastasiavela@berkeley.edu>
2018-08-27 14:32:14 -0700

    revert chevron

--- js/chevron.js
+++ js/chevron.js
@@ -1,6 +1,6 @@
-window.onload = function() {
-	document.getElementById("chevron").onclick = function(){
-		document.getElementById("menu").style.marginLeft = "0px";
-		document.getElementById("chevron").style.display = "none";
-	};
-}
+$(document).ready(function(){
+    $('#chevron').on('click touchstart', function() {
+    	$("#menu").css("margin-left", "0px");
+    	$("#chevron").css("display", "none");
+    });
+});
